0afb80d089c3fbca9691104f81db93258b044640
chris <cnt@uw.edu>
2013-02-06 23:58:22 -0800

    some refactoring.

--- dummyDevice.js
+++ dummyDevice.js
@@ -23,8 +23,6 @@ function Device(listen_port) {
 
   this.addEventHandler('getCode',getAppEvent); 
 }
-Device.prototype.init = function() {
-}
 Device.prototype.advertize = function() {
   //broadcast on a specified multicast address/port that you exist
   //TODO: fill in
@@ -38,15 +36,18 @@ Device.prototype.manageHTTPRequest = function(request,response) {
   //response: http response
     
   //parse request
-  var req_args = url.parse(request.url,true).query;
-  if (typeof(this.events[req_args.action]) === 'function' ) {
-    this.events[req_args.action](response);
-  }
   //TODO: figure out how to handle POST data portion of request
-  
-  //bad request!
-  response.writeHead(400, {'Content-Type': 'text/plain'});
-  response.end();
+  //      for now nothing needs it so it has been left out.
+  var req_args = url.parse(request.url,true).query;
+  var eventfn = this.events[req_args.cmd](response);
+  var event_data = null; //this is where the post data goes.
+  if (typeof(eventfn) === 'function' ) {
+    eventfn(event_data, response);
+  } else {
+    //bad request!
+    response.writeHead(400, {'Content-Type': 'text/plain'});
+    response.end();
+  }
   
 }
 Device.prototype.addEventHandler = function(event_name, handler) {
@@ -55,17 +56,17 @@ Device.prototype.addEventHandler = function(event_name, handler) {
   this.events[event_name] = handler;
 }
 
-function getAppEvent(response) {
+function getAppEvent(event_data, response) {
   //gets the app code and sends it in the response body
   //response: the HTTP response
   
+  //TODO: (vlp) make this an async call so we don't have to wait on IO
   var file =  fs.readFileSync(app_code_path,'utf8');
   //TODO: change content-type to javascript
   response.writeHead(200, {'Content-Type': 'text/plain'});
   console.log(file);
   response.end(file);
 }
-function 
 
 ///////////////////////////////////// MAIN ////////////////////////////////////
 d = new Device(8080);
